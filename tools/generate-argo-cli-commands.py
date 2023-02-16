326b01373cf7f4bd226c6f4c4e39e180f6109dd4
MDavidson <mdavidson@linz.govt.nz>
2023-02-16 13:17:52 +1300

    fix: format python file with black

--- tools/generate-argo-cli-commands.py
+++ tools/generate-argo-cli-commands.py
@@ -6,109 +6,113 @@ from linz_logger import get_log
 
 # nb: CHANGE if working from a different source
 SOURCE = "s3://linz-data-lake-raster-prod/"
-PRODUCERS = ["AAM NZ",
-            "Aerial Surveys",
-            "Beca",
-            "GeoSmart",
-            "Landpro",
-            "Maxar",
-            "NZ Aerial Mapping",
-            "Recon",
-            "Terralink International",
-            "UAV Mapping NZ"]
-LICENSORS = ["Ashburton District Council",
-            "Auckland Council",
-            "BOPLASS",
-            "Bay of Plenty Regional Council",
-            "Buller District Council",
-            "Canterbury Aerial Imagery Consortium (CAI)",
-            "Carterton District Council",
-            "Central Hawke's Bay District Council",
-            "Central Otago District Council",
-            "Chatham Islands Council",
-            "Christchurch City Council",
-            "Clutha District Council",
-            "CoLAB",
-            "Department of Conservation",
-            "Dunedin City Council",
-            "Environment Canterbury",
-            "Environment Southland",
-            "Far North District Council",
-            "Gisborne District Council",
-            "Gore District Council",
-            "Greater Wellington Regional Council",
-            "Grey District Council",
-            "Hamilton City Council",
-            "Hastings District Council",
-            "Hauraki District Council",
-            "Hawke's Bay Local Authority Shared Services (HB LASS)",
-            "Hawke's Bay Regional Council",
-            "Horizons Regional Council",
-            "Horowhenua District Council",
-            "Hurunui District Council",
-            "Hutt City Council",
-            "Invercargill City Council",
-            "Kaikōura District Council",
-            "Kaipara District Council",
-            "Kawerau District Council",
-            "Kāpiti Coast District Council",
-            "Mackenzie District Council",
-            "Manawatū District Council",
-            "Manawatū-Whanganui LASS",
-            "Marlborough District Council",
-            "Masterton District Council",
-            "Matamata-Piako District Council",
-            "Maxar Technologies",
-            "Ministry of Primary Industries",
-            "NZ Aerial Mapping",
-            "Napier City Council",
-            "Nelson City Council",
-            "New Plymouth District Council",
-            "Northland Regional Council",
-            "Ōpōtiki District Council",
-            "Ōtorohanga District Council",
-            "Otago Regional Council",
-            "Palmerston North City Council",
-            "Porirua City Council",
-            "Queenstown-Lakes District Council",
-            "Rangitīkei District Council",
-            "Rotorua District Council",
-            "Ruapehu District Council",
-            "Selwyn District Council",
-            "Sinergise",
-            "South Taranaki District Council",
-            "South Waikato District Council",
-            "South Wairarapa District Council",
-            "Southland District Council",
-            "Stratford District Council",
-            "Taranaki Regional Council",
-            "Tararua District Council",
-            "Tasman District Council",
-            "Taupō District Council",
-            "Tauranga City Council",
-            "Terralink International",
-            "Thames-Coromandel District Council",
-            "Timaru District Council",
-            "Toitū Te Whenua Land Information New Zealand",
-            "Upper Hutt City Council",
-            "Waikato District Council",
-            "Waikato Regional Aerial Photography Service (WRAPS)",
-            "Waikato Regional Council",
-            "Waimakariri District Council",
-            "Waimate District Council",
-            "Waipā District Council",
-            "Wairoa District Council",
-            "Waitaki District Council",
-            "Waitomo District Council",
-            "Waka Kotahi",
-            "Wellington City Council",
-            "West Coast Regional Council",
-            "Western Bay of Plenty District Council",
-            "Westland District Council",
-            "Whakatāne District Council",
-            "Whanganui District Council",
-            "Whangārei District Council",]
-SCALES = ["500","1000","2000","5000","10000"]
+PRODUCERS = [
+    "AAM NZ",
+    "Aerial Surveys",
+    "Beca",
+    "GeoSmart",
+    "Landpro",
+    "Maxar",
+    "NZ Aerial Mapping",
+    "Recon",
+    "Terralink International",
+    "UAV Mapping NZ",
+]
+LICENSORS = [
+    "Ashburton District Council",
+    "Auckland Council",
+    "BOPLASS",
+    "Bay of Plenty Regional Council",
+    "Buller District Council",
+    "Canterbury Aerial Imagery Consortium (CAI)",
+    "Carterton District Council",
+    "Central Hawke's Bay District Council",
+    "Central Otago District Council",
+    "Chatham Islands Council",
+    "Christchurch City Council",
+    "Clutha District Council",
+    "CoLAB",
+    "Department of Conservation",
+    "Dunedin City Council",
+    "Environment Canterbury",
+    "Environment Southland",
+    "Far North District Council",
+    "Gisborne District Council",
+    "Gore District Council",
+    "Greater Wellington Regional Council",
+    "Grey District Council",
+    "Hamilton City Council",
+    "Hastings District Council",
+    "Hauraki District Council",
+    "Hawke's Bay Local Authority Shared Services (HB LASS)",
+    "Hawke's Bay Regional Council",
+    "Horizons Regional Council",
+    "Horowhenua District Council",
+    "Hurunui District Council",
+    "Hutt City Council",
+    "Invercargill City Council",
+    "Kaikōura District Council",
+    "Kaipara District Council",
+    "Kawerau District Council",
+    "Kāpiti Coast District Council",
+    "Mackenzie District Council",
+    "Manawatū District Council",
+    "Manawatū-Whanganui LASS",
+    "Marlborough District Council",
+    "Masterton District Council",
+    "Matamata-Piako District Council",
+    "Maxar Technologies",
+    "Ministry of Primary Industries",
+    "NZ Aerial Mapping",
+    "Napier City Council",
+    "Nelson City Council",
+    "New Plymouth District Council",
+    "Northland Regional Council",
+    "Ōpōtiki District Council",
+    "Ōtorohanga District Council",
+    "Otago Regional Council",
+    "Palmerston North City Council",
+    "Porirua City Council",
+    "Queenstown-Lakes District Council",
+    "Rangitīkei District Council",
+    "Rotorua District Council",
+    "Ruapehu District Council",
+    "Selwyn District Council",
+    "Sinergise",
+    "South Taranaki District Council",
+    "South Waikato District Council",
+    "South Wairarapa District Council",
+    "Southland District Council",
+    "Stratford District Council",
+    "Taranaki Regional Council",
+    "Tararua District Council",
+    "Tasman District Council",
+    "Taupō District Council",
+    "Tauranga City Council",
+    "Terralink International",
+    "Thames-Coromandel District Council",
+    "Timaru District Council",
+    "Toitū Te Whenua Land Information New Zealand",
+    "Upper Hutt City Council",
+    "Waikato District Council",
+    "Waikato Regional Aerial Photography Service (WRAPS)",
+    "Waikato Regional Council",
+    "Waimakariri District Council",
+    "Waimate District Council",
+    "Waipā District Council",
+    "Wairoa District Council",
+    "Waitaki District Council",
+    "Waitomo District Council",
+    "Waka Kotahi",
+    "Wellington City Council",
+    "West Coast Regional Council",
+    "Western Bay of Plenty District Council",
+    "Westland District Council",
+    "Whakatāne District Council",
+    "Whanganui District Council",
+    "Whangārei District Council",
+]
+SCALES = ["500", "1000", "2000", "5000", "10000"]
 
 command_list = []
 
@@ -119,6 +123,7 @@ def _format_date(date: str) -> str:
     month = f"{int(fd_lst[0]):02}"
     day = f"{int(fd_lst[1]):02}"
     return f"{year}-{month}-{day}"
+
 
 def _validate_licensor(licensor: str) -> Optional[str]:
     if licensor in LICENSORS:
@@ -135,6 +140,7 @@ def _validate_licensor(licensor: str) -> Optional[str]:
         return licensor.replace(" and ", ";")
     return None
 
+
 def _validate_producer(producer: str) -> Optional[str]:
     if producer in PRODUCERS:
         return producer
@@ -148,10 +154,12 @@ def _validate_producer(producer: str) -> Optional[str]:
         return "Landpro"
     return None
 
+
 def _validate_scale(scale: str) -> Optional[str]:
     if scale in SCALES:
         return scale
     return None
+
 
 with open("./imagery-standardising-parameters-bulk-process.csv", "r") as csv_file:
     reader = csv.reader(csv_file)
@@ -193,7 +201,7 @@ with open("./imagery-standardising-parameters-bulk-process.csv", "r") as csv_fil
 
         file_name = row[ind_title].lower().replace(" ", "-").replace(".", "_")
 
-        params =  {
+        params = {
             "source": row[ind_source],
             "target": row[ind_target],
             "scale": _validate_scale(row[ind_scale]),
@@ -216,7 +224,7 @@ with open("./imagery-standardising-parameters-bulk-process.csv", "r") as csv_fil
                 "skipped: invalid producer",
                 producer=row[ind_producer],
                 source=row[ind_source],
-                title=row[ind_title],      
+                title=row[ind_title],
             )
             continue
 
@@ -228,7 +236,7 @@ with open("./imagery-standardising-parameters-bulk-process.csv", "r") as csv_fil
                 title=row[ind_title],
             )
             continue
-        
+
         if not params["scale"]:
             get_log().warning(
                 "skipped: invalid scale",
@@ -237,7 +245,7 @@ with open("./imagery-standardising-parameters-bulk-process.csv", "r") as csv_fil
                 title=row[ind_title],
             )
             continue
-        
+
         command_list.append(command.format(file_name))
 
         with open(f"./{file_name}.yaml", "w", encoding="utf-8") as output:
