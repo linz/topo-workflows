bce9a4f7e01025770b6b6c4f9a3e2b49d1e6d4b1
MDavidson <mdavidson@linz.govt.nz>
2023-02-17 15:56:13 +1300

    fix: issue with tasman deleted in linz-imagery

--- tools/test-workflows.sh
+++ tools/test-workflows.sh
@@ -4,5 +4,4 @@
 # argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./gisborne_2012-2013_0.4m.yaml
 # argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./bay-of-plenty_2011-2012_0.25m.yaml
 # argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./auckland_2010-2012_0.5m.yaml
-# argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./tasman_2001-2002_1m.yaml
 # argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./waikato_2012-2013_0.5m.yaml
