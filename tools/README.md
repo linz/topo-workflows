b3703d64e721784f4be0a11048f345cb93164672
Megan Davidson <33814653+MDavidson17@users.noreply.github.com>
2023-08-11 09:04:02 +1200

    feat: tool to generate elevation argo commands TDE-783 (#144)
    sample output parameters yaml:
    ```
    "source": "s3://linz-elevation-staging/lidar2/Auckland_South_LiDAR_(2016-2017)/Auckland_South_LiDAR_1m_DSM_(2016-2017)/"
    "target": "s3://linz-elevation/auckland/auckland-south_2016-2017/dsm_1m/2193/"
    "title": "Auckland South LiDAR 1m DSM (2016-2017)"
    "description": "Digital Surface Model within the Auckland South region captured in 2016-2017."
    "start-datetime": "2016-09-22"
    "end-datetime": "2017-06-22"
    "scale": "10000"
    "source-epsg": "2193"
    "target-epsg": "2193"
    "compression": "dem_lerc"
    "retile": "true"
    "validate": "false"
    "group": "1"
    "licensor": "Auckland Council"
    "licensor-list": ""
    "producer": "AAM NZ"
    "producer-list": ""
    ```
    
    sample output submit script:
    ```
    #!/bin/bash
    
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./mackenzie-2015-dem-1m.yaml --generate-name mackenzie-2015-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./mackenzie-2015-dsm-1m.yaml --generate-name mackenzie-2015-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./queenstown-2016-dem-1m.yaml --generate-name queenstown-2016-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./queenstown-2016-dsm-1m.yaml --generate-name queenstown-2016-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./blenheim-2014-dem-1m.yaml --generate-name blenheim-2014-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./blenheim-2014-dsm-1m.yaml --generate-name blenheim-2014-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./reporoa-and-upper-piako-river-2019-dem-1m.yaml --generate-name reporoa-and-upper-piako-river-2019-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./reporoa-and-upper-piako-river-2019-dsm-1m.yaml --generate-name reporoa-and-upper-piako-river-2019-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./otago-2016-dem-1m.yaml --generate-name otago-2016-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./otago-2016-dsm-1m.yaml --generate-name otago-2016-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./motueka-river-valley-2018-2019-dem-1m.yaml --generate-name motueka-river-valley-2018-2019-dem-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./motueka-river-valley-2018-2019-dsm-1m.yaml --generate-name motueka-river-valley-2018-2019-dsm-1m
    argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./whangarei-heads-2016-dem-1m.yaml --generate-name whangarei-heads-2016-dem-1m
    
    # tauranga-and-coast-2015-dem-1m.yaml not written to bash as further action required: ASC FILES
    # tauranga-and-coast-2015-dsm-1m.yaml not written to bash as further action required: ASC FILES
    # west-coast-and-hauraki-plains-2015-dem-1m.yaml not written to bash as further action required: ASC FILES
    # west-coast-and-hauraki-plains-2015-dsm-1m.yaml not written to bash as further action required: ASC FILES
    
    ```

---
+++ tools/README.md
@@ -0,0 +1,34 @@
+# Tools
+
+This folder contains single use scripts which have been used to assist in automating argo jobs.  
+The scripts should be stored in this folder if they may become useful again in the future.
+
+## generate-argo-cli-commands-elevation.py
+
+**Date:** 31/07/2023
+
+**Related Jira Tickets:** [TDE-783](https://toitutewhenua.atlassian.net/browse/TDE-783?atlOrigin=eyJpIjoiODhlMWFkYTVlYzhiNGJjYTgwOTI4OWVjNmQ3YzczMjciLCJwIjoiaiJ9)
+
+**Description:**  
+This script sets up for the automated processing of numerous elevation datasets using the argo cli.
+
+**Instructions:**
+
+1. Run:
+
+```bash
+cd ./tools
+python3 generate-argo-cli-commands-elevation.py
+```
+
+**Output:**
+
+- **region-year-datatype-scale.yaml:** workflow parameters for this dataset
+- **standardise-publish.sh:** bash script to 'deploy' argo workflows  
+   **nb: the commented lines at the end of this file detail the datasets not run due to know issues.**
+
+**Submitting:**
+
+```bash
+sh standardise-publish.sh
+```
---
+++ tools/generate-argo-cli-commands-elevation.py
@@ -0,0 +1,118 @@
+import csv
+from typing import List, Dict, Tuple
+import yaml
+
+PARAMETERS_CSV = "./data/elevation-31-07-23.csv"
+
+COMMAND = "argo submit ~/dev/topo-workflows/workflows/imagery/standardising-publish-import.yaml -n argo -f ./{0}.yaml --generate-name {1}\n"
+
+
+def _index_csv(header: List[str]) -> Dict[str, int]:
+    ind = {}
+    ind["comments"] = header.index("Comments")
+    ind["title"] = header.index("title")
+    ind["licensor"] = header.index("licensor")
+    ind["producer"] = header.index("producer")
+    ind["startdate"] = header.index("start-datetime")
+    ind["enddate"] = header.index("end-datetime")
+    ind["verticalEPSG"] = header.index("vertical-datum")
+    ind["horizontalEPSG"] = header.index("horizontal-datum")
+    ind["source"] = header.index("source")
+    ind["target"] = header.index("target")
+    ind["inputscale"] = header.index("input-scale")
+    ind["outputscale"] = header.index("output-scale")
+    ind["description"] = header.index("description")
+    return ind
+
+
+def _add_licensor(row: List[str], index: Dict[str, int]) -> Dict[str, str]:
+    licensor = row[index["licensor"]]
+    if ";" in licensor:
+        return {"licensor-list": licensor, "licensor": ""}
+    else:
+        return {"licensor": licensor, "licensor-list": ""}
+
+
+def _add_producer(row: List[str], index: Dict[str, int]) -> Dict[str, str]:
+    producer = row[index["producer"]]
+    if ";" in producer:
+        return {"producer-list": producer, "producer": ""}
+    else:
+        return {"producer": producer, "producer-list": ""}
+
+
+def _write_params(params: Dict[str, str], file: str) -> None:
+    with open(f"./{file}.yaml", "w", encoding="utf-8") as output:
+        yaml.dump(
+            params,
+            output,
+            default_flow_style=False,
+            default_style='"',
+            sort_keys=False,
+            allow_unicode=True,
+            width=1000,
+        )
+
+
+def _valid_params(params: Dict[str, str]) -> Tuple[bool, str]:
+    if params["comments"] != "":
+        return (False, params["comments"])
+    for param in params:
+        if "TODO" in params[param]:
+            return (False, "TODO Noted")
+    return (True, "")
+
+def _tmp_target_edit(target: str) -> str:
+    return target.replace("s3://linz-elevation/", "s3://linz-workflow-artifacts/linz-elevation/")
+
+
+with open(PARAMETERS_CSV, "r") as csv_file:
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    index = _index_csv(header)
+
+    parameter_list = []
+    not_valid = []
+
+    for row in reader:
+        if not row[index["target"]]:
+            continue
+        params = {
+            "comments": row[index["comments"]],
+            "source": row[index["source"]],
+            "target": _tmp_target_edit(row[index["target"]]),
+            "title": row[index["title"]],
+            "description": row[index["description"]],
+            "start-datetime": row[index["startdate"]],
+            "end-datetime": row[index["enddate"]],
+            "scale": row[index["outputscale"]],
+            "source-epsg": row[index["horizontalEPSG"]],
+            "target-epsg": "2193",
+            "compression": "dem_lerc",
+            "retile": "true",
+            "validate": "false",
+            "group": "5",
+        }
+
+        params = {**params, **_add_licensor(row, index)}
+        params = {**params, **_add_producer(row, index)}
+
+        file_name = row[index["target"]].split("/")[-4:-2]
+        file_name = f"{file_name[0]}-{file_name[1]}"
+        formatted_file_name = file_name.replace("_", "-").replace(".", "-")
+
+        valid = _valid_params(params)
+        
+        if not valid[0]:
+            not_valid.append(f"# {formatted_file_name}.yaml not written to bash as further action required: {valid[1]}\n")
+        else:
+            parameter_list.append(COMMAND.format(formatted_file_name, formatted_file_name))
+
+        del params["comments"]
+        _write_params(params, formatted_file_name)
+
+    with open("./standardise-publish.sh", "w") as script:
+        script.write("#!/bin/bash\n\n")
+        script.writelines(parameter_list)
+        script.writelines("\n\n\n")
+        script.writelines(not_valid)
