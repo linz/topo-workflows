b4583b1b450a482d3c5063c2c8ddfde8f4243e8f
Paul Fouquet <pfouquet@linz.govt.nz>
2023-10-12 10:52:46 +1300

    wip

---
+++ config/app.ts
@@ -0,0 +1,14 @@
+import { App } from 'cdk8s';
+
+
+const app = new App();
+
+async function main(): Promise<void> {
+//   const ns = new NamespaceChart(app, 'namespace', { namespace: 'argo' });
+
+//     const fluentBit = new FluentBit(app, 'fluentBit', {});
+//   fluentBit.addDependency(ns);
+//   app.synth();
+// }
+
+main();
---
+++ config/charts/argo.semaphores.ts
@@ -0,0 +1,11 @@
+import {Chart} from 'cdk8s'
+
+export class ArgoSemaphore extends Chart {
+    constructor(
+        scope: Construct,
+    id: string,
+    props: ChartProps,
+  ){
+
+  }
+}
