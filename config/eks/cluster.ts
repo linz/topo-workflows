a172003efac1e131f9e808a176e8ed8735be17cd
Blayne Chard <bchard@linz.govt.nz>
2023-10-19 15:09:39 +1300

    fix: roll back to 2.93 to fix a missing stackname error

--- config/eks/cluster.ts
+++ config/eks/cluster.ts
@@ -2,21 +2,27 @@ import { KubectlV27Layer } from '@aws-cdk/lambda-layer-kubectl-v27';
 import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { InstanceType, IVpc, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Cluster, ClusterLoggingTypes, IpFamily, KubernetesVersion, NodegroupAmiType } from 'aws-cdk-lib/aws-eks';
+import { Role } from 'aws-cdk-lib/aws-iam';
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 interface EksClusterProps extends StackProps {}
 
 export class LinzEksCluster extends Stack {
-  tempBucket: Bucket;
-  vpc: IVpc;
+  /** Version of EKS to use, this must be aligned to the `kubectlLayer` */
   version = KubernetesVersion.V1_27;
+  /** Argo needs a temporary bucket to store objects */
+  tempBucket: Bucket;
+
+  vpc: IVpc;
+
   cluster: Cluster;
 
   constructor(scope: Construct, id: string, props: EksClusterProps) {
     super(scope, id, props);
 
-    this.tempBucket = new Bucket(this, 'Artifacts', {
+    this.tempBucket = new Bucket(this, 'Scratch', {
+      /** linz-workflows-scratch */
       bucketName: `linz-${id.toLowerCase()}-scratch`,
       removalPolicy: RemovalPolicy.RETAIN,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
@@ -38,33 +44,29 @@ export class LinzEksCluster extends Stack {
       vpc: this.vpc,
       defaultCapacity: 0,
       vpcSubnets: [{ subnetType: SubnetType.PRIVATE_WITH_EGRESS }],
+      /** This must align to Cluster version: {@link version} */
       kubectlLayer: new KubectlV27Layer(this, 'KubeCtlLayer'),
+      /** To prevent IP exhaustion when running huge workflows run using ipv6 */
       ipFamily: IpFamily.IP_V6,
       clusterLogging: [ClusterLoggingTypes.API, ClusterLoggingTypes.CONTROLLER_MANAGER, ClusterLoggingTypes.SCHEDULER],
     });
 
-    const nodeGroup = this.cluster.addNodegroupCapacity('cluster-default', {
-      instanceTypes: [
-        new InstanceType('c7i.large'),
-        new InstanceType('c7a.large'),
-        new InstanceType('c6i.large'),
-        new InstanceType('c6a.large'),
-        new InstanceType('m6i.large'),
-        new InstanceType('m6a.large'),
-        new InstanceType('c5.large'),
-        new InstanceType('c5a.large'),
-      ],
+    const nodeGroup = this.cluster.addNodegroupCapacity('ClusterDefault', {
       /**
        * c6i.large: ~$70/month, t3.small: ~$12/month.
        * Compare instance types and costs at https://instances.vantage.sh/
        * Instances are requested in order listed.
        * https://docs.aws.amazon.com/eks/latest/userguide/managed-node-groups.html#managed-node-group-capacity-types
        **/
-
+      instanceTypes: ['c6i.large', 'c6a.large'].map((f) => new InstanceType(f)),
       minSize: 2,
       amiType: NodegroupAmiType.BOTTLEROCKET_X86_64,
       subnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
     });
     this.tempBucket.grantReadWrite(nodeGroup.role);
+
+    // Grant the AWS Admin user ability to view the cluster
+    const accountAdminRole = Role.fromRoleName(this, 'AccountAdminRole', 'AccountAdminRole');
+    this.cluster.awsAuth.addMastersRole(accountAdminRole);
   }
 }
