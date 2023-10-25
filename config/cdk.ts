ff21089c4cfea0f4b027d31f23b885cc611933dc
Alice Fage <afage@linz.govt.nz>
2023-10-26 09:24:25 +1300

    fix: import rds stack

--- config/cdk.ts
+++ config/cdk.ts
@@ -3,12 +3,15 @@ import { App } from 'aws-cdk-lib';
 import { CLUSTER_NAME } from './constants';
 import { LinzEksCluster } from './eks/cluster';
 
+import { ArgoRdsStack } from './rds/argo.rds.js';
+
 const app = new App();
 
 async function main(): Promise<void> {
   new LinzEksCluster(app, CLUSTER_NAME, {
     env: { region: 'ap-southeast-2', account: process.env.CDK_DEFAULT_ACCOUNT },
   });
+  new ArgoRdsStack(app, 'ArgoDbNonProd', { env: { region: 'ap-southeast-2', account: process.env.CDK_DEFAULT_ACCOUNT } });
 
   app.synth();
 }
