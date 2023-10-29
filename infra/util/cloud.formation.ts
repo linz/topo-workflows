ed87f4798651bdf61facc7df4084ed4a8f611277
Blayne Chard <bchard@linz.govt.nz>
2023-10-30 10:30:05 +1300

    refactor: simplify cfn output logic

--- infra/util/cloud.formation.ts
+++ infra/util/cloud.formation.ts
@@ -3,21 +3,12 @@ import { CloudFormation } from '@aws-sdk/client-cloudformation';
 export async function getCfnOutputs(stackName: string): Promise<Record<string, string>> {
   const cfn = new CloudFormation();
   const searchStacks = await cfn.describeStacks({ StackName: stackName });
-  cfn.listExports;
   const outputs: Record<string, string> = {};
-  const stacks = (searchStacks && searchStacks.Stacks) || [];
-  const stack = stacks.find((s) => s.StackName === stackName);
+  const stack = searchStacks?.Stacks?.find((s) => s.StackName === stackName);
+  if (stack?.Outputs == null) throw new Error(`Unable to find stack "${stackName}"`);
 
-  if (!stack) {
-    throw new Error(`Unable to find stack "${stackName}"`);
-  }
-  if (!stack.Outputs) {
-    throw new Error(`There is no output for stack "${stackName}"`);
-  }
   stack.Outputs.forEach(({ OutputKey, OutputValue }) => {
-    if (OutputKey && OutputValue) {
-      outputs[OutputKey] = OutputValue;
-    }
+    if (OutputKey != null && OutputValue != null) outputs[OutputKey] = OutputValue;
   });
   return outputs;
 }
