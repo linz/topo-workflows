8854600b9d687dd717eddd949d2f91124206db0a
Blayne Chard <bchard@linz.govt.nz>
2023-11-01 09:39:16 +1300

    refactor: apply stricter tsconfig

--- infra/cdk8s.ts
+++ infra/cdk8s.ts
@@ -1,28 +1,21 @@
 import { App } from 'cdk8s';
 
-import { ArgoSemaphore } from './charts/argo.semaphores';
-import { ArgoWorkflows } from './charts/argo.workflows';
-import { Cloudflared } from './charts/cloudflared';
-import { FluentBit } from './charts/fluentbit';
-import { Karpenter, KarpenterProvisioner } from './charts/karpenter';
-import { CoreDns } from './charts/kube-system.coredns';
-import { CfnOutputKeys, ClusterName } from './constants';
-import { getCfnOutputs } from './util/cloud.formation';
-import { fetchSsmParameters } from './util/ssm';
+import { ArgoSemaphore } from './charts/argo.semaphores.js';
+import { ArgoWorkflows } from './charts/argo.workflows.js';
+import { Cloudflared } from './charts/cloudflared.js';
+import { FluentBit } from './charts/fluentbit.js';
+import { Karpenter, KarpenterProvisioner } from './charts/karpenter.js';
+import { CoreDns } from './charts/kube-system.coredns.js';
+import { CfnOutputKeys, ClusterName, validateKeys } from './constants.js';
+import { getCfnOutputs } from './util/cloud.formation.js';
+import { fetchSsmParameters } from './util/ssm.js';
 
 const app = new App();
 
 async function main(): Promise<void> {
   // Get cloudformation outputs
   const cfnOutputs = await getCfnOutputs(ClusterName);
-  const missingKeys = [
-    ...Object.values(CfnOutputKeys.Karpenter),
-    ...Object.values(CfnOutputKeys.FluentBit),
-    ...Object.values(CfnOutputKeys.Argo),
-  ].filter((f) => cfnOutputs[f] == null);
-  if (missingKeys.length > 0) {
-    throw new Error(`Missing CloudFormation Outputs for keys ${missingKeys.join(', ')}`);
-  }
+  validateKeys(cfnOutputs);
 
   const ssmConfig = await fetchSsmParameters({
     // Config for Cloudflared to access argo-server
@@ -38,33 +31,33 @@ async function main(): Promise<void> {
   new ArgoSemaphore(app, 'semaphore', {});
   const coredns = new CoreDns(app, 'dns', {});
   const fluentbit = new FluentBit(app, 'fluentbit', {
-    saName: cfnOutputs[CfnOutputKeys.FluentBit.ServiceAccountName],
+    saName: cfnOutputs[CfnOutputKeys.FluentBitServiceAccountName],
     clusterName: ClusterName,
   });
   fluentbit.addDependency(coredns);
 
   const karpenter = new Karpenter(app, 'karpenter', {
     clusterName: ClusterName,
-    clusterEndpoint: cfnOutputs[CfnOutputKeys.Karpenter.ClusterEndpoint],
-    saName: cfnOutputs[CfnOutputKeys.Karpenter.ServiceAccountName],
-    saRoleArn: cfnOutputs[CfnOutputKeys.Karpenter.ServiceAccountRoleArn],
-    instanceProfile: cfnOutputs[CfnOutputKeys.Karpenter.DefaultInstanceProfile],
+    clusterEndpoint: cfnOutputs[CfnOutputKeys.ClusterEndpoint],
+    saName: cfnOutputs[CfnOutputKeys.KarpenterServiceAccountName],
+    saRoleArn: cfnOutputs[CfnOutputKeys.KarpenterServiceAccountRoleArn],
+    instanceProfile: cfnOutputs[CfnOutputKeys.KarpenterDefaultInstanceProfile],
   });
 
   const karpenterProvisioner = new KarpenterProvisioner(app, 'karpenter-provisioner', {
     clusterName: ClusterName,
-    clusterEndpoint: cfnOutputs[CfnOutputKeys.Karpenter.ClusterEndpoint],
-    saName: cfnOutputs[CfnOutputKeys.Karpenter.ServiceAccountName],
-    saRoleArn: cfnOutputs[CfnOutputKeys.Karpenter.ServiceAccountRoleArn],
-    instanceProfile: cfnOutputs[CfnOutputKeys.Karpenter.DefaultInstanceProfile],
+    clusterEndpoint: cfnOutputs[CfnOutputKeys.ClusterEndpoint],
+    saName: cfnOutputs[CfnOutputKeys.KarpenterServiceAccountName],
+    saRoleArn: cfnOutputs[CfnOutputKeys.KarpenterServiceAccountRoleArn],
+    instanceProfile: cfnOutputs[CfnOutputKeys.KarpenterDefaultInstanceProfile],
   });
 
   karpenterProvisioner.addDependency(karpenter);
 
   new ArgoWorkflows(app, 'argo-workflows', {
     clusterName: ClusterName,
-    saName: cfnOutputs[CfnOutputKeys.Argo.RunnerServiceAccountName],
-    tempBucketName: cfnOutputs[CfnOutputKeys.Argo.TempBucketName],
+    saName: cfnOutputs[CfnOutputKeys.ArgoRunnerServiceAccountName],
+    tempBucketName: cfnOutputs[CfnOutputKeys.TempBucketName],
   });
 
   new Cloudflared(app, 'cloudflared', {
--- infra/constants.ts
+++ infra/constants.ts
@@ -3,17 +3,32 @@ export const ClusterName = 'Workflows';
 
 /* CloudFormation Output to access from CDK8s */
 export const CfnOutputKeys = {
-  Karpenter: {
-    ServiceAccountName: 'KarpenterServiceAccountName',
-    ServiceAccountRoleArn: 'KarpenterServiceAccountRoleArn',
-    ClusterEndpoint: 'ClusterEndpoint',
-    DefaultInstanceProfile: 'DefaultInstanceProfile',
-  },
-  FluentBit: {
-    ServiceAccountName: 'FluentBitServiceAccountName',
-  },
-  Argo: {
-    RunnerServiceAccountName: 'ArgoRunnerServiceAccountName',
-    TempBucketName: 'TempBucketName',
-  },
-};
+  ClusterEndpoint: 'ClusterEndpoint',
+
+  KarpenterServiceAccountName: 'KarpenterServiceAccountName',
+  KarpenterServiceAccountRoleArn: 'KarpenterServiceAccountRoleArn',
+  KarpenterDefaultInstanceProfile: 'KarpenterDefaultInstanceProfile',
+
+  FluentBitServiceAccountName: 'FluentBitServiceAccountName',
+
+  ArgoRunnerServiceAccountName: 'ArgoRunnerServiceAccountName',
+
+  TempBucketName: 'TempBucketName',
+} as const;
+
+/** The list of possible keys */
+export type ICfnOutputKeys = keyof typeof CfnOutputKeys;
+/** A map containing a key value pair for every possible CfnOutputKey */
+export type CfnOutputMap = Record<ICfnOutputKeys, string>;
+
+/**
+ *  Assert that all the keys in this Record contains all the expected CfnOutputKeys
+ *
+ * @see {@link CfnOutputKeys}
+ */
+export function validateKeys(cfnOutputs: Record<string, string>): asserts cfnOutputs is CfnOutputMap {
+  const missingKeys = Object.values(CfnOutputKeys).filter((f) => cfnOutputs[f] == null);
+  if (missingKeys.length > 0) {
+    throw new Error(`Missing CloudFormation Outputs for keys ${missingKeys.join(', ')}`);
+  }
+}
