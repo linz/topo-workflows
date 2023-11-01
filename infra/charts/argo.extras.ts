17841e6a8a3dc06c1861cb6824cb9126f86b7051
Blayne Chard <bchard@linz.govt.nz>
2023-11-01 17:16:33 +1300

    docs: explain the split between extras and workflows

--- infra/charts/argo.extras.ts
+++ infra/charts/argo.extras.ts
@@ -5,7 +5,11 @@ import { Construct } from 'constructs';
 import { applyDefaultLabels } from '../util/labels.js';
 
 /**
- * Configuration for the workflows themselves trying to keep this a little seperate from the argo server setup
+ * Extra configuration for the workflows themselves
+ *
+ * With the goal that the [ArgoWorkflows](./argo.workflows.ts) chart is focused more on the server setup and
+ * {@link ArgoExtras} to be focused on the workflows running
+ *
  */
 export class ArgoExtras extends Chart {
   constructor(
@@ -18,13 +22,18 @@ export class ArgoExtras extends Chart {
     new kplus.ConfigMap(this, 'semaphores', {
       metadata: { name: 'semaphores' },
       data: {
-        standardising: '2', // Limit of how many standardising workflow instances can run at the same time
-        bulk: '4', // Limit of how many bulk workflow instances can run at the same time
-        bulkcopy: '8', // Limit of how many publish-copy workflow instances can run at the same time
-        basemaps_import: '10', // Limit of how many basemaps import workflow instances can run at the same time
+        // Limit of how many standardising workflow instances can run at the same time
+        standardising: '2',
+        // Limit of how many bulk workflow instances can run at the same time
+        bulk: '4',
+        // Limit of how many publish-copy workflow instances can run at the same time
+        bulkcopy: '8',
+        // Limit of how many basemaps import workflow instances can run at the same time
+        basemaps_import: '10',
       },
     });
 
+    // Secrets used by the workflows
     for (const s of props.secrets) {
       const secret = new kplus.Secret(this, 'secret-' + s.name, { metadata: { name: s.name } });
       for (const [key, value] of Object.entries(s.data)) {
