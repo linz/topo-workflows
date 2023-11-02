b63a82bcdde2207fdb2b07c5cd35702ddf940bab
Alice Fage <afage@linz.govt.nz>
2023-11-02 13:52:44 +1300

    fix: environment variable reference

--- infra/cdk.ts
+++ infra/cdk.ts
@@ -11,7 +11,7 @@ async function main(): Promise<void> {
   new LinzEksCluster(app, ClusterName, {
     env: { region: 'ap-southeast-2', account: process.env['CDK_DEFAULT_ACCOUNT'] },
   });
-  new ArgoRdsStack(app, DbName, { env: { region: 'ap-southeast-2', account: process.env.CDK_DEFAULT_ACCOUNT } });
+  new ArgoRdsStack(app, DbName, { env: { region: 'ap-southeast-2', account: process.env['CDK_DEFAULT_ACCOUNT'] } });
 
   app.synth();
 }
