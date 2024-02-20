1b4f89a7cbb182fceba7b31d49ffbe8ee7878dd1
Alice Fage <afage@linz.govt.nz>
2024-02-21 10:58:59 +1300

    fix: update elevation workflows

--- tools/generate-argo-cli-commands-elevation.py
+++ tools/generate-argo-cli-commands-elevation.py
@@ -6,7 +6,7 @@ from unidecode import unidecode
 
 PARAMETERS_CSV = "./data/elevation-argo-parameters.csv"
 
-COMMAND = "argo submit workflows/raster/standardising-publish-import-elevation.yaml -n argo -f ./{0}.yaml --generate-name test-{1}\n"
+COMMAND = "argo submit workflows/raster/standardising-publish-import-elevation.yaml -n argo -f ./tools/{0}.yaml --generate-name elevation-{1}\n"
 
 TARGET = "linz-workflows-scratch/elevation"
 
