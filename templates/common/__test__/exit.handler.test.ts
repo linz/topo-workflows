c41966b6069fff40f9dd6c2aa01fd92d8584ce2b
Paul Fouquet <pfouquet@linz.govt.nz>
2024-08-29 13:25:53 +1200

    refactor: exit.handler.test should be TS

--- templates/common/__test__/exit.handler.test.ts
+++ templates/common/__test__/exit.handler.test.ts
@@ -2,34 +2,33 @@ import assert from 'node:assert';
 import fs from 'node:fs';
 import { describe, it } from 'node:test';
 
+type scriptContext = { workflowParameters: string; workflowStatus: string };
+
 /**
  * Read the workflow YAML file and create a function from the script inside.
  * replacing {{ inputs.* }} with ctx
  *
  * @param {*} ctx
- * @returns Function that requires a `context` and `core` parameter
+ * @returns Function that requires a `context`
  */
-function createTestFunction(ctx) {
+function runTestFunction(ctx: scriptContext): void {
   const func = fs.readFileSync('./templates/common/exit.handler.yml', 'utf-8').split('source: |')[1];
-
+  if (!func) {
+    throw new Error('No script found in the workflow');
+  }
   const newFunc = func
     // Replace inputs
     .replace('{{= inputs.parameters.workflow_parameters }}', `${ctx.workflowParameters ?? '[]'}`)
-    .replace('{{inputs.parameters.workflow_status}}', `${ctx.workflowStatus ?? 'Failed'}`)
-    .split('\n')
-    .join('\n');
-  return new Function('context', newFunc);
-}
-
-function runScript(ctx) {
-  return createTestFunction(ctx)();
+    .replace('{{inputs.parameters.workflow_status}}', `${ctx.workflowStatus ?? 'Failed'}`);
+  // eslint-disable-next-line @typescript-eslint/no-implied-eval
+  new Function(newFunc)();
 }
 
 describe('exit handler script template', () => {
   it('should log workflow status and parameters', (t) => {
     const spy = t.mock.method(console, 'log');
 
-    runScript({
+    runTestFunction({
       workflowParameters: JSON.stringify([
         { name: 'source', value: 's3://linz-topographic-upload/abc/', description: 'Source bucket' },
         { name: 'ticket', value: 'GDE-123', description: 'JIRA Ticket' },
@@ -39,7 +38,8 @@ describe('exit handler script template', () => {
 
     assert.equal(spy.mock.callCount(), 1);
 
-    const logOutputDict = JSON.parse(spy.mock.calls[0].arguments[0]);
+    const logOutputDict = JSON.parse(String(spy.mock.calls[0]?.arguments[0])) as { time: number };
+
     // override time
     logOutputDict.time = 1724037007216;
 
