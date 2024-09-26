eb72fd793dd4ea888a0628b0fbef32792bc52fdb
Victor Engmark <victor@engmark.name>
2024-09-27 11:13:08 +1200

    build: Update EKS to version 29

--- infra/charts/argo.workflows.ts
+++ infra/charts/argo.workflows.ts
@@ -1,5 +1,5 @@
 import { Chart, ChartProps, Duration, Helm } from 'cdk8s';
-import { Secret } from 'cdk8s-plus-28';
+import { Secret } from 'cdk8s-plus-29';
 import { Construct } from 'constructs';
 
 import { ArgoDbName, ArgoDbUser, DefaultRegion } from '../constants.js';
