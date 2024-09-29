956e893c5a394aa7cab4f082b143fd1ed511eb94
Victor Engmark <vengmark@linz.govt.nz>
2024-09-30 12:07:03 +1300

    build: Update EKS from 28 to 29 TDE-1255 (#777)
    #### Motivation
    
    Keep up to date with AWS, which is dropping standard support for 28 on
    November 26, 2024.
    
    #### Checklist
    
    - [ ] Tests updated (N/A)
    - [x] Docs updated
    - [x] Issue linked in Title
    
    ---------
    
    Co-authored-by: Blayne Chard <bchard@linz.govt.nz>

--- infra/eks/cluster.ts
+++ infra/eks/cluster.ts
@@ -1,4 +1,4 @@
-import { KubectlV28Layer } from '@aws-cdk/lambda-layer-kubectl-v28';
+import { KubectlV29Layer } from '@aws-cdk/lambda-layer-kubectl-v29';
 import { Aws, CfnOutput, Duration, RemovalPolicy, SecretValue, Size, Stack, StackProps } from 'aws-cdk-lib';
 import * as chatbot from 'aws-cdk-lib/aws-chatbot';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
@@ -44,7 +44,7 @@ export class LinzEksCluster extends Stack {
   /* Cluster ID */
   id: string;
   /** Version of EKS to use, this must be aligned to the `kubectlLayer` */
-  version = KubernetesVersion.of('1.28');
+  version = KubernetesVersion.of('1.29');
   /** Argo needs a database for workflow archive */
   argoDb: DatabaseInstance;
   /** Argo needs a temporary bucket to store objects */
@@ -72,7 +72,7 @@ export class LinzEksCluster extends Stack {
       defaultCapacity: 0,
       vpcSubnets: [{ subnetType: SubnetType.PRIVATE_WITH_EGRESS }],
       /** This must align to Cluster version: {@link version} */
-      kubectlLayer: new KubectlV28Layer(this, 'KubeCtlLayer'),
+      kubectlLayer: new KubectlV29Layer(this, 'KubeCtlLayer'),
       /** To prevent IP exhaustion when running huge workflows run using ipv6 */
       ipFamily: IpFamily.IP_V6,
       clusterLogging: [ClusterLoggingTypes.API, ClusterLoggingTypes.CONTROLLER_MANAGER, ClusterLoggingTypes.SCHEDULER],
