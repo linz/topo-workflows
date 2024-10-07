89b0e1ff7e5c67201da6cf5956dcaf70173e69ae
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2024-10-08 09:36:36 +1300

    test: use yaml parser to extract wf script TDE-1280 (#796)
    #### Motivation
    
    Using a yaml parser is safer than splitting on an expected string in the
    Workflow file.
    
    #### Modification
    
    - integrate https://github.com/eemeli/yaml library
    - modify the way to extract a script from a task within a workflow file
    
    #### Checklist
    
    - [x] Tests updated
    - [x] Docs updated
    - [x] Issue linked in Title

--- templates/common/__test__/exit.handler.test.ts
+++ templates/common/__test__/exit.handler.test.ts
@@ -2,6 +2,30 @@ import assert from 'node:assert';
 import fs from 'node:fs';
 import { describe, it } from 'node:test';
 
+import YAML from 'yaml';
+
+/**
+ * Workflow template type (partial)
+ */
+type WorkflowTemplate = { spec: { templates: { name: string; script: { source: string } }[] } };
+
+/**
+ * Extract the script of the task named `taskName` from the `workflowTemplate`.
+ *
+ * @param workflow Workflow template
+ * @param taskName the task to extract the script from
+ * @returns the script of the task
+ */
+function getScript(workflow: WorkflowTemplate, taskName: string): string {
+  const template = workflow?.spec?.templates?.find((f) => f.name === taskName);
+  if (template == null) throw new Error(`Task ${taskName} not found in the workflow`);
+
+  const source = template.script?.source;
+  if (source == null) throw new Error(`Task ${taskName} has no script`);
+
+  return source;
+}
+
 /**
  * Read the workflow YAML file and create a function from the script inside.
  * replacing {{ inputs.* }} with ctx
@@ -9,12 +33,12 @@ import { describe, it } from 'node:test';
  * @param ctx
  */
 function runTestFunction(ctx: { workflowParameters: string; workflowStatus: string }): void {
-  const func = fs.readFileSync('./templates/common/exit.handler.yml', 'utf-8').split('source: |')[1];
-  if (!func) {
-    throw new Error('No script found in the workflow');
-  }
-  const newFunc = func
-    // Replace inputs
+  const wfRaw = fs.readFileSync('./templates/common/exit.handler.yml', 'utf-8');
+  const wfTemplate = YAML.parse(wfRaw) as WorkflowTemplate;
+  const script = getScript(wfTemplate, 'main');
+
+  // Replace inputs with ctx
+  const newFunc = script
     .replace('{{= inputs.parameters.workflow_parameters }}', `${ctx.workflowParameters ?? '[]'}`)
     .replace('{{inputs.parameters.workflow_status}}', `${ctx.workflowStatus ?? 'Failed'}`);
   // eslint-disable-next-line @typescript-eslint/no-implied-eval
