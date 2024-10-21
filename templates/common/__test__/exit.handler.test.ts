b4d2dbcebf50ad39e8e07554c1bddeeffe73ec8e
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2024-10-21 15:12:46 +1300

    fix: get-location should output the s3 archive workflow directory TDE-1280 (#788)
    #### Motivation
    
    Since upgrading our cluster to EKS 1.29, we experience a different
    behaviour when calling `{{pod.name}}`. This PR changes the way
    `get-location` build the s3 archive location for the current workflow.
    
    #### Modification
    
    - modify the `get-location` template script behaviour
    - refactorisation to allow testing
    
    NOTE: test have been done using `console.log()` because exporting a
    script containing imports declaration seems very tricky.
    
    #### Checklist
    
    - [x] Tests updated
    - [x] Docs updated
    - [x] Issue linked in Title
    
    ---------
    
    Co-authored-by: Victor Engmark <vengmark@linz.govt.nz>

--- templates/common/__test__/exit.handler.test.ts
+++ templates/common/__test__/exit.handler.test.ts
@@ -1,61 +1,22 @@
 import assert from 'node:assert';
-import fs from 'node:fs';
 import { describe, it } from 'node:test';
 
-import YAML from 'yaml';
-
-/**
- * Workflow template type (partial)
- */
-type WorkflowTemplate = { spec: { templates: { name: string; script: { source: string } }[] } };
-
-/**
- * Extract the script of the task named `taskName` from the `workflowTemplate`.
- *
- * @param workflow Workflow template
- * @param taskName the task to extract the script from
- * @returns the script of the task
- */
-function getScript(workflow: WorkflowTemplate, taskName: string): string {
-  const template = workflow?.spec?.templates?.find((f) => f.name === taskName);
-  if (template == null) throw new Error(`Task ${taskName} not found in the workflow`);
-
-  const source = template.script?.source;
-  if (source == null) throw new Error(`Task ${taskName} has no script`);
-
-  return source;
-}
-
-/**
- * Read the workflow YAML file and create a function from the script inside.
- * replacing {{ inputs.* }} with ctx
- *
- * @param ctx
- */
-function runTestFunction(ctx: { workflowParameters: string; workflowStatus: string }): void {
-  const wfRaw = fs.readFileSync('./templates/common/exit.handler.yml', 'utf-8');
-  const wfTemplate = YAML.parse(wfRaw) as WorkflowTemplate;
-  const script = getScript(wfTemplate, 'main');
-
-  // Replace inputs with ctx
-  const newFunc = script
-    .replace('{{= inputs.parameters.workflow_parameters }}', `${ctx.workflowParameters ?? '[]'}`)
-    .replace('{{inputs.parameters.workflow_status}}', `${ctx.workflowStatus ?? 'Failed'}`);
-  // eslint-disable-next-line @typescript-eslint/no-implied-eval
-  new Function(newFunc)();
-}
+import { runTestFunction } from './function.helper.js';
 
 describe('exit handler script template', () => {
   it('should log workflow status and parameters', (t) => {
     const spy = t.mock.method(console, 'log');
 
-    runTestFunction({
-      workflowParameters: JSON.stringify([
-        { name: 'source', value: 's3://linz-topographic-upload/abc/', description: 'Source bucket' },
-        { name: 'ticket', value: 'GDE-123', description: 'JIRA Ticket' },
-      ]),
-      workflowStatus: `Succeeded`,
-    });
+    runTestFunction('./templates/common/exit.handler.yml', [
+      {
+        toReplace: '{{= inputs.parameters.workflow_parameters }}',
+        replaceWith: JSON.stringify([
+          { name: 'source', value: 's3://linz-topographic-upload/abc/', description: 'Source bucket' },
+          { name: 'ticket', value: 'GDE-123', description: 'JIRA Ticket' },
+        ]),
+      },
+      { toReplace: '{{inputs.parameters.workflow_status}}', replaceWith: 'Succeeded' },
+    ]);
 
     assert.equal(spy.mock.callCount(), 1);
 
