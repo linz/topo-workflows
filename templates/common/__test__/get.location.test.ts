9420533f2c624138d70d6fc91be42e741e56f1de
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2024-10-21 15:12:46 +1300

    fix: get-location should output the s3 archive workflow directory TDE-1280 (#788)
    #### Motivation
    
    Since upgrading our cluster to EKS 1.29, we experience a different
    behaviour when calling `{{pod.name}}`. This PR changes the way
    `get-location` build the s3 archive location for the current workflow.
    
    #### Modification
    
    - modify the `get-location` template script behaviour
    - refactorisation to allow testing
    
    NOTE: test have been done using `console.log()` because exporting a
    script containing imports declaration seems very tricky.
    
    #### Checklist
    
    - [x] Tests updated
    - [x] Docs updated
    - [x] Issue linked in Title
    
    ---------
    
    Co-authored-by: Victor Engmark <vengmark@linz.govt.nz>

---
+++ templates/common/__test__/get.location.test.ts
@@ -0,0 +1,47 @@
+import assert from 'node:assert';
+import fs from 'node:fs';
+import { describe, it } from 'node:test';
+
+import { runTestFunction } from './function.helper.js';
+
+describe('get-location script template', () => {
+  it('should output workflow artifact location', (t) => {
+    const spy = t.mock.method(console, 'log');
+    const shimRequired: string[] = [];
+    runTestFunction(
+      './templates/common/get.location.yml',
+      [
+        {
+          toReplace: `JSON.parse(process.env['ARGO_TEMPLATE'])`,
+          replaceWith: JSON.stringify({
+            archiveLocation: {
+              s3: {
+                key: '2024-10/02-test-get-location-29l4x/test-get-location-29l4x-get-location-3125883809',
+                bucket: 'linz-workflows-scratch',
+              },
+            },
+          }),
+        },
+      ],
+      (req) => {
+        if (req !== 'node:fs') throw new Error('Failed');
+        shimRequired.push(req);
+        return fs;
+      },
+    );
+    assert.equal(spy.mock.callCount(), 1);
+
+    const logOutputDict = JSON.parse(String(spy.mock.calls[0]?.arguments[0])) as { time: number };
+
+    assert.deepEqual(logOutputDict, {
+      time: logOutputDict.time,
+      level: 20,
+      pid: 1,
+      msg: 'Workflow:Location',
+      location: 's3://linz-workflows-scratch/2024-10/02-test-get-location-29l4x/',
+      bucket: 'linz-workflows-scratch',
+      key: '2024-10/02-test-get-location-29l4x',
+    });
+    assert.deepEqual(shimRequired, ['node:fs']);
+  });
+});
