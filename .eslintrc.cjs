298892b8ca034a9298a30e9acf69a4d7b3d53864
Blayne Chard <bchard@linz.govt.nz>
2025-03-18 15:47:33 +1300

    feat: enable yaml linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,19 @@ const cfg = {
 const testOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.includes('.test.ts')));
 testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
 
+cfg.overrides.push({
+  /** Overrides for YAML */
+  files: ['*.yaml', '*.yml'],
+  extends: ['plugin:yml/standard'],
+  rules: {
+    // Prettier uses single quotes so be consistent
+    'yml/quotes': ['error', { prefer: 'single' }],
+    // Prettier will reformat this back into a single line
+    'yml/block-sequence-hyphen-indicator-newline': ['off'],
+    // Prettier will sometimes convert these to multiline, so be safe and always use multi
+    'yml/block-sequence': ['error', { singleline: 'always' }],
+  },
+  parser: 'yaml-eslint-parser',
+});
+
 module.exports = cfg;
