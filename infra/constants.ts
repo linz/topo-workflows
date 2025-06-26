9a04b87c4f63e897691d20a2dc6b2bd39f92c4bb
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2025-06-26 15:18:28 +1200

    fix: undo dev changes
    Co-authored-by: paulfouquet <86932794+paulfouquet@users.noreply.github.com>

--- infra/constants.ts
+++ infra/constants.ts
@@ -1,5 +1,5 @@
 /** Cluster name */
-export const ClusterName = 'WorkflowsDev';
+export const ClusterName = 'Workflows';
 /** LINZ conventional name for Argo Workflows artifact bucket */
 export const ScratchBucketName = `linz-${ClusterName.toLowerCase()}-scratch`;
 /** Argo Database Instance name */
