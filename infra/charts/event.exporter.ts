9e860499ecf86f8611e4baf6cf7263f342d4729a
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2025-06-26 15:03:16 +1200

    feat: upgrade event-exporter TDE-1559 (#1037)
    ### Motivation
    
    Keeping `eventexporter` up to date.
    
    ### Modifications
    
    Update chart and app version of event exporter. 
    
    
    ### Verification
    
    Successfully deployed on NonProd cluster
    
    <!-- TODO: Say how you tested your changes. -->

--- infra/charts/event.exporter.ts
+++ infra/charts/event.exporter.ts
@@ -1,77 +1,37 @@
-import { Chart, ChartProps } from 'cdk8s';
-import {
-  ApiResource,
-  ClusterRole,
-  ConfigMap,
-  Deployment,
-  ImagePullPolicy,
-  Namespace,
-  ServiceAccount,
-  Volume,
-} from 'cdk8s-plus-32';
+import { Chart, ChartProps, Helm } from 'cdk8s';
+import { Namespace } from 'cdk8s-plus-32';
 import { Construct } from 'constructs';
 
 import { applyDefaultLabels } from '../util/labels.js';
 
-/** `resmoio/kubernetes-event-exporter` image version */
-const version = 'v1.5';
+/** This is the chart version vs app version which is 1.7.0
+ *  https://github.com/bitnami/charts/blob/dbcee8a0b5e14163163aecc2935b908943ec5bdb/bitnami/kubernetes-event-exporter/Chart.yaml
+ */
+const chartVersion = '3.5.6';
+
+const appVersion = '1.7.0';
 
 export class EventExporter extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps) {
-    super(scope, id, applyDefaultLabels(props, 'event-exporter', version, 'event-exporter', 'event-exporter'));
+    super(scope, id, applyDefaultLabels(props, 'event-exporter', appVersion, 'event-exporter', 'event-exporter'));
 
     new Namespace(this, 'namespace', {
       metadata: { name: props.namespace },
     });
 
-    const serviceAccount = new ServiceAccount(this, 'event-exporter-sa', {
-      metadata: { name: 'event-exporter', namespace: props.namespace },
-    });
-
-    // https://cdk8s.io/docs/latest/plus/cdk8s-plus-32/rbac/#role
-    const clusterRole = new ClusterRole(this, 'event-exporter-cr', {
-      metadata: { name: 'event-exporter' },
-    });
-    clusterRole.allowRead(ApiResource.custom({ apiGroup: '*', resourceType: '*' }));
-    // create a ClusterRoleBinding
-    clusterRole.bind(serviceAccount);
-
-    const cm = new ConfigMap(this, 'event-exporter-cfg', {
-      metadata: { name: 'event-exporter-cfg', namespace: props.namespace },
-      data: {
-        //FIXME do like cloudflared
-        'config.yaml': `
-logLevel: error
-logFormat: json
-route:
-  routes:
-    - match:
-        - receiver: "dump"
-receivers:
-  - name: "dump"
-    stdout: {}`,
-      },
-    });
-
-    new Deployment(this, 'event-exporter', {
-      metadata: { name: 'event-exporter', namespace: props.namespace },
-      replicas: 1,
-      podMetadata: {
-        labels: { app: 'event-exporter', version: 'v1' },
-      },
-      containers: [
-        {
-          image: `ghcr.io/resmoio/kubernetes-event-exporter:${version}`,
-          imagePullPolicy: ImagePullPolicy.IF_NOT_PRESENT,
-          args: ['-conf=/data/config.yaml'],
-          name: 'event-exporter',
-          volumeMounts: [{ path: '/data', volume: Volume.fromConfigMap(this, 'cfg', cm, { name: 'cfg' }) }],
-          securityContext: { allowPrivilegeEscalation: false },
+    new Helm(this, 'kubernetes-event-exporter', {
+      chart: 'oci://registry-1.docker.io/bitnamicharts/kubernetes-event-exporter',
+      namespace: 'event-exporter',
+      version: chartVersion,
+      values: {
+        config: {
+          logLevel: 'error',
+          logFormat: 'json',
         },
-      ],
-      serviceAccount: serviceAccount,
-      securityContext: { ensureNonRoot: true },
-      automountServiceAccountToken: true,
+        serviceAccount: {
+          automountServiceAccountToken: true,
+        },
+      },
     });
   }
 }
