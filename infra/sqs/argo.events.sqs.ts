0c21a6de072eb189bc23ae4accdee8e4ca3fcfd0
Alice Fage <afage@linz.govt.nz>
2026-02-10 11:37:32 +1300

    fix: props type

--- infra/sqs/argo.events.sqs.ts
+++ infra/sqs/argo.events.sqs.ts
@@ -1,13 +1,12 @@
-import { CfnOutput, Duration, RemovalPolicy, Stack } from 'aws-cdk-lib';
-
-import { Construct } from 'constructs';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
 import { CfnOutputKeys, ScratchBucketName } from '../constants.ts';
 
 export class ArgoEventsSQS extends Stack {
   argoEventsSqs: Queue;
-  constructor(scope: Construct, id: string, props: {}) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     this.argoEventsSqs = new Queue(this, id, {
