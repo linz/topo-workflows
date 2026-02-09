b8776d9846db9e79350462b8af09324f656334ed
Alice Fage <afage@linz.govt.nz>
2026-02-10 10:16:57 +1300

    refactor: event stack props

--- infra/sqs/argo.events.sqs.ts
+++ infra/sqs/argo.events.sqs.ts
@@ -6,17 +6,17 @@ import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { ArgoEventsSQSName, CfnOutputKeys } from '../constants.ts';
 
 export class ArgoEventsSQS extends Stack {
+  argoEventsSqs: Queue;
   constructor(scope: Construct, id: string, props: {}) {
     super(scope, id, props);
 
-    const sqs = new Queue(this, ArgoEventsSQSName, {
+    this.argoEventsSqs = new Queue(this, ArgoEventsSQSName, {
       visibilityTimeout: Duration.seconds(30),
       queueName: ArgoEventsSQSName,
     });
-    sqs.applyRemovalPolicy(RemovalPolicy.RETAIN);
-
+    this.argoEventsSqs.applyRemovalPolicy(RemovalPolicy.RETAIN);
     new CfnOutput(this, CfnOutputKeys.ArgoEventsSQSArn, {
-      value: sqs.queueArn,
+      value: this.argoEventsSqs.queueArn,
       exportName: CfnOutputKeys.ArgoEventsSQSArn,
     });
   }
