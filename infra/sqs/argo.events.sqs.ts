8697a070d014ea0b004281d38efa31a0167a656f
Alice Fage <afage@linz.govt.nz>
2026-02-10 13:24:02 +1300

    feat: allow sqs to receive events from scratch bucket

--- infra/sqs/argo.events.sqs.ts
+++ infra/sqs/argo.events.sqs.ts
@@ -1,8 +1,10 @@
 import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
-import { CfnOutputKeys } from '../constants.ts';
+import { CfnOutputKeys, ScratchBucketName } from '../constants.ts';
 
 interface ArgoEventsSQSProps extends StackProps {
   /** Whether to enable Slack alerts for the database */
@@ -18,7 +20,17 @@ export class ArgoEventsSQS extends Stack {
       visibilityTimeout: Duration.seconds(30),
       queueName: props.queueName,
     });
+
     this.argoEventsSqs.applyRemovalPolicy(RemovalPolicy.RETAIN);
+
+    this.argoEventsSqs.grantSendMessages(
+      new ServicePrincipal('s3.amazonaws.com', {
+        conditions: {
+          ArnLike: { 'aws:SourceArn': Bucket.fromBucketName(this, 'Scratch', ScratchBucketName).bucketArn },
+        },
+      }),
+    );
+
     new CfnOutput(this, CfnOutputKeys.ArgoEventsSQSArn, {
       value: this.argoEventsSqs.queueArn,
       exportName: CfnOutputKeys.ArgoEventsSQSArn,
