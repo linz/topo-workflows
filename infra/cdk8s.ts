7d23b119ff2125541672bb12495632ec6dc97219
Alice Fage <afage@linz.govt.nz>
2026-02-19 09:07:01 +1300

    chore: rename publish to copy-odr in cdk and cdk8s

--- infra/cdk8s.ts
+++ infra/cdk8s.ts
@@ -102,7 +102,7 @@ async function main(): Promise<void> {
   new ArgoEvents(app, 'argo-events', {
     namespace: 'argo-events',
     saName: cfnOutputs[CfnOutputKeys.ArgoEventsServiceAccountName],
-    sqsPublishQueueName: getNameFromArn(cfnOutputs[CfnOutputKeys.ScratchPublishSqsQueueArn]),
+    sqsCopyOdrQueueName: getNameFromArn(cfnOutputs[CfnOutputKeys.ScratchCopyOdrSqsQueueArn]),
   });
 
   new ArgoExtras(app, 'argo-extras', {
--- infra/eks/cluster.ts
+++ infra/eks/cluster.ts
@@ -272,7 +272,7 @@ export class LinzEksCluster extends Stack {
   }
 
   createArgoEventsServiceAccount(): void {
-    const sqsPublishQueueArn = Fn.importValue(CfnOutputKeys.ScratchPublishSqsQueueArn);
+    const sqsCopyOdrQueueArn = Fn.importValue(CfnOutputKeys.ScratchCopyOdrSqsQueueArn);
     const argoEventsNamespaceName = 'argo-events';
     const argoEventsSaName = 'event-sa';
     const argoEventsNs = this.cluster.addManifest('ArgoEventsNameSpace', {
@@ -289,7 +289,7 @@ export class LinzEksCluster extends Stack {
     argoEventsSa.role.addToPrincipalPolicy(
       new PolicyStatement({
         actions: ['sqs:GetQueueUrl', 'sqs:ReceiveMessage', 'sqs:DeleteMessage', 'sqs:GetQueueAttributes'],
-        resources: [sqsPublishQueueArn],
+        resources: [sqsCopyOdrQueueArn],
       }),
     );
 
